29d97ceb2ee77cce9a631312cdee5bf1467278bc
techhtml <apes0123@gmail.com>
2018-07-06 15:54:51 +0900

    add PWA Examples

---
+++ pwa/registerServiceWorker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "test-cashe-1";
+
+const urlsToCache = [
+  '/pwa/',
+  '/pwa/test.css',
+  '/pwa/test.js'
+]
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CHCHE_NAME)
+      .then(function(cache) {
+        console.log("CACHE 등록 완료");
+        return cache.addAll(urlsToCache);
+      })
+  );
+})
---
+++ pwa/test.js
@@ -0,0 +1,3 @@
+const main = document.querySelector("#main");
+
+main.innerHTML = "이것이 보인다면 스크립트가 정상적으로 로딩된 것입니다";
